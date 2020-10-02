fd03c7952304304e70060608c0ca9717fb691394
Simon Huang <me@simonh.io>
2020-10-01 18:31:41 -0700

    Added SSL to Google Cloud SQL connection

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,10 +1,20 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { Pool } from 'pg';
+import fs from 'fs';
 import typeDefs from '../../apollo/type-defs';
 import resolvers from '../../apollo/resolvers';
 import PostgresDB from '../../datasource/postgres';
 
-const postgres = new PostgresDB({ pool: new Pool() });
+const pgConfig = {
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('/googleSQLSSL/server-ca.pem').toString(),
+    key: fs.readFileSync('/googleSQLSSL/client-key.pem').toString(),
+    cert: fs.readFileSync('/googleSQLSSL/client-cert.pem').toString(),
+  },
+};
+
+const postgres = new PostgresDB({ pool: new Pool(pgConfig) });
 
 const apolloServer = new ApolloServer({
   typeDefs,
