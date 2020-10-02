05d6817c5eea4de136085a932fcb8f51f3bae515
Simon Huang <me@simonh.io>
2020-10-01 18:48:12 -0700

    Removed ssl files, using google cloud integration

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,20 +1,10 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { Pool } from 'pg';
-import fs from 'fs';
 import typeDefs from '../../apollo/type-defs';
 import resolvers from '../../apollo/resolvers';
 import PostgresDB from '../../datasource/postgres';
 
-const pgConfig = {
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('/googleSQLSSL/server-ca.pem').toString(),
-    key: fs.readFileSync('/googleSQLSSL/client-key.pem').toString(),
-    cert: fs.readFileSync('/googleSQLSSL/client-cert.pem').toString(),
-  },
-};
-
-const postgres = new PostgresDB({ pool: new Pool(pgConfig) });
+const postgres = new PostgresDB({ pool: new Pool() });
 
 const apolloServer = new ApolloServer({
   typeDefs,
