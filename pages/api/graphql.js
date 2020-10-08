2b18e92e7b26a37466e47567f70dc555e6ca3a16
Simon Huang <me@simonh.io>
2020-10-08 01:10:36 -0700

    Trying ssl on server

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -4,7 +4,16 @@ import typeDefs from '../../apollo/type-defs';
 import resolvers from '../../apollo/resolvers';
 import PostgresDB from '../../datasource/postgres';
 
-const postgres = new PostgresDB({ pool: new Pool() });
+const pgConfig = {
+  ssl: process.env.NODE_ENV === 'production' ? {
+    rejectUnauthorized: false,
+    ca: process.env.SERVER_CA,
+    key: process.env.CLIENT_KEY,
+    cert: process.env.CLIENT_CERT,
+  } : null,
+};
+
+const postgres = new PostgresDB({ pool: new Pool(pgConfig) });
 
 const apolloServer = new ApolloServer({
   typeDefs,
