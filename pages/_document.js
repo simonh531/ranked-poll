fcca08948c2e583ba122c6e35d1d899ff4914073
Simon Huang <me@simonh.io>
2020-10-24 16:48:21 -0700

    Google analytics added

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,17 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* eslint-disable-next-line react/no-danger */}
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l] = w[l] || []{'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5655TMW');
+          `,
+          }}
+          />
           <meta charSet="UTF-8" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -25,6 +36,13 @@ class MyDocument extends Document {
           <meta name="twitter:card" content="summary_large_image" />
         </Head>
         <body>
+          {/* eslint-disable-next-line react/no-danger */}
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5655TMW"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          `,
+          }}
+          />
           <Main />
           <NextScript />
         </body>
