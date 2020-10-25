7f890fd8c95d5b31f3ca227b53f3d198a1cdb680
Simon Huang <me@simonh.io>
2020-10-24 18:26:34 -0700

    Changed to react-gtm-module

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   ApolloProvider, ApolloClient, InMemoryCache,
 } from '@apollo/client';
 import { Provider } from 'next-auth/client';
+
+import TagManager from 'react-gtm-module';
 
 import Layout from '../components/layout';
 import '../style/style.css';
@@ -12,7 +14,15 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const tagManagerArgs = {
+  gtmId: 'GTM-5655TMW',
+};
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <Provider session={pageProps.session}>
       <ApolloProvider client={client}>
