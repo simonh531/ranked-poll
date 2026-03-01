253c9a512ee5378a7060cf6ed9d6285379f13d11
simonh531 <9309088+simonh531@users.noreply.github.com>
2026-02-28 18:36:40 -0800

    Fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import perfectionist from "eslint-plugin-perfectionist";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 // Use the official ESLint core defineConfig instead of tseslint.config
