c743b8c0af671f994e1d5cf07ec150aecef515c9
simonh531 <9309088+simonh531@users.noreply.github.com>
2026-06-19 11:16:03 -0700

    Reduce use client

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,20 +1,26 @@
-"use client"
-
-import { Separator as SeparatorPrimitive } from "@base-ui/react/separator"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
+
+interface SeparatorProps extends React.ComponentProps<"div"> {
+  orientation?: "horizontal" | "vertical"
+}
 
 function Separator({
   className,
   orientation = "horizontal",
   ...props
-}: SeparatorPrimitive.Props) {
+}: SeparatorProps) {
   return (
-    <SeparatorPrimitive
+    <div
+      role="separator"
+      aria-orientation={orientation}
       data-slot="separator"
-      orientation={orientation}
       className={cn(
-        "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
+        "shrink-0 bg-border",
+        orientation === "horizontal"
+          ? "h-px w-full"
+          : "w-px self-stretch",
         className
       )}
       {...props}
